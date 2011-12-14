e24f5de666d0209a118636f7d6267f9a19383ef1
Jari Aalto <jari.aalto@cante.net>
2011-12-14 23:57:40 +0200

    twittering-mode-epkg-compile.el: new

---
+++ epackage/twittering-mode-epkg-compile.el
@@ -0,0 +1,8 @@
+(dolist (file
+         '("twittering-mode.el"))
+  (if (and (boundp 'verbose)
+	   verbose)
+      (message "Byte Compile %s" file))
+  (if (file-exists-p file)
+      (byte-compile-file file)
+    (message "** Byte compile error. Not found: %s" file)))
