e645c3c437a0714d6a6873d839df1a29fd5a6bb8
Jari Aalto <jari.aalto@cante.net>
2011-12-15 00:08:39 +0200

    twittering-mode-epkg-examples.el: new

---
+++ epackage/twittering-mode-epkg-examples.el
@@ -0,0 +1,7 @@
+;; Prevent loading this file. Study the examples.
+(error "twittering-mode-epkg-examples.el is not a configuration file.")
+
+;; Prevent twittering-mode ask for authorization everytime you run it
+(setq twittering-use-master-password t)
+
+;; End of file
