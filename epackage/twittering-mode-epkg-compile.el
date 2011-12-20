9263106c819b1d1327f13c208d0abe1daec82136
Jari Aalto <jari.aalto@cante.net>
2011-12-20 23:51:25 +0200

    rm twittering-mode-epkg-compile.el: only one file
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- epackage/twittering-mode-epkg-compile.el
+++
@@ -1,8 +0,0 @@
-(dolist (file
-         '("twittering-mode.el"))
-  (if (and (boundp 'verbose)
-	   verbose)
-      (message "Byte Compile %s" file))
-  (if (file-exists-p file)
-      (byte-compile-file file)
-    (message "** Byte compile error. Not found: %s" file)))
